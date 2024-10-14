d5af4969c9bba61c700e2f38f1556c8eebfd2870
mamehamu <ryusuarepants@gmail.com>
2024-10-14 19:27:15 +0900

    Initial Commit

---
+++ Luigi MansionUITests/Luigi_MansionUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Luigi_MansionUITestsLaunchTests.swift
+//  Luigi MansionUITests
+//
+//  Created by rikuya on 2024/10/14.
+//
+
+import XCTest
+
+final class Luigi_MansionUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
